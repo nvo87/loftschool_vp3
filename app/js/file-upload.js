46ca60585e4751313e4684ff6b871c45240d20f4
skama <skama@mail.ru>
2015-07-08 14:15:44 +0300

    add script file-upload

--- app/js/file-upload.js
+++ app/js/file-upload.js
@@ -1,3 +1,30 @@
 ;var fileUpload = (function(){
-	console.log('Я файлуплоад!');
+
+	var wrapper = $('.field-file-upload'),
+		input = wrapper.find('input'),
+		fakeInput = wrapper.find('.fake-input-text'),
+		fileAPI = (window.File && window.FileReader && window.FileList && window.Blob) ? true : false;
+
+	return {
+		init: function(){
+			this.setupListeners();
+		},
+		setupListeners: function(){
+			input.on('change', getFileName);
+		}
+	};
+
+	function getFileName(e){
+		e.preventDefault();
+		var fileName;
+		if (fileAPI && input[0].files[0]) {
+			fileName = input[0].files[0].name;
+		} else {
+			fileName = input.val().replace('C:\\fakepath\\', '');
+		}
+		if (!fileName.length)
+			return;
+		fakeInput.text(fileName);
+	}
+
 })();
