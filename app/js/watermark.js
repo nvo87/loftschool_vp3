c8dbf27e6f2386788cc83e181429f8bec132d975
nvo87 <nvo87@yandex.ru>
2015-07-10 18:58:23 +0300

    Подправил watermark.js чтобы работало

--- app/js/watermark.js
+++ app/js/watermark.js
@@ -8,8 +8,7 @@
     });
 
     watermark.on('mousemove', function(){
-        $('.settings__position-x').text(parseInt(watermark.offset().left - parent.offset().left));
-        $('.settings__position-y').text(parseInt(watermark.offset().top - parent.offset().top));
-        watermark.css('cursor', 'move');
+        $('#x-axis').val(parseInt(watermark.offset().left - parent.offset().left));
+        $('#y-axis').val(parseInt(watermark.offset().top - parent.offset().top));
     });
 }());
