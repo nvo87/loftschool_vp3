c58400606506b2e287e74d45c401d4508bf1f681
Nunin Alexey <webdesc@gmail.com>
2015-07-12 19:30:59 +0300

    file-upload & render load images for window

--- app/js/main.js
+++ app/js/main.js
@@ -1,34 +1,3 @@
 jQuery(document).ready(function($) {
-	
-/*Стилизованный input=file*/
-		// отслеживаем изменение инпута file
-	$('#bg-file').change(function(){
-		// Если файл прикрепили то заносим значение value в переменную
-		var $this = $(this),
-			fileResult = $this.val(),
-			inputText = $this.parent().find('.file-load__wrapper').find('input');
-			console.log(fileResult);
-			console.log(inputText);
-		// И дальше передаем значение в инпут, который под загрузчиком
-				inputText.val(fileResult);
-	});
-
-
-
-//После добавления ответа от сервера
-/*
-	$(function () {
-	    'use strict';
-	    // Change this to the location of your server-side upload handler:
-	    var url = './php/upload.php';
-	    $('#file').fileupload({
-	        url: url,
-	        dataType: 'json',
-	        success: function (data) {
-	           $('#project-img-path').val(data.name);
-	           console.log(data.name);
-	        }
-	    })
-	});*/
 
 });
