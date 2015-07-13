c3eee5721da71f2c38b377257b93676e487f0b75
nvo87 <nvo87@yandex.ru>
2015-07-13 18:43:53 +0300

    file-upload сделан одной функцией в js, php - решена проблема с путями, если сайт лежит не в корне. Начал масштабирование

--- app/js/file-upload.js
+++ app/js/file-upload.js
@@ -1,85 +1,33 @@
-/*$(function () {
-    'use strict';
+	$(function () {
+		'use strict';
+		// Change this to the location of your server-side upload handler:
+		var url = './php/upload.php',
+			formFile = $('.file-load__input-file');
 
-    // Change this to the location of your server-side upload handler:
-    var url = './php/upload/',
-        formFile = $('.file-load__input-file');
-    formFile.fileupload({
-        url: url,
-        allowedTypes:"png,gif,jpg,jpeg",
-        dataType: 'json',
-        add: function (e, data) {
-            data.submit();
-        },
-        done: function (e, data) {
-            var fakeInput = $(this).parent().find('.file-load__input-text'),
-                fakeInputID = $(this).attr('id');
-             $.each(data.result.files, function (index, file) {
-                fakeInput.val(file.name);
-                fileLoadWindow(file.url, fakeInputID);
-            });
-        }
-    });
+		formFile.fileupload({
+			url: url,
+			allowedTypes:"png,gif,jpg,jpeg",
+			dataType: 'json',
+			success: function (data) {
+				var _this=this.fileInputClone, 			//получение объекта, инициализировавшего вызов fileupload
+					fakeInput = _this.parent().find('.file-load__input-text'),
+					fakeInputID = _this.attr('id');		//id, чтобы определить куда грузить картинку, в bg или в wm
 
-    function fileLoadWindow(url, img){
-        if (img === 'bg-file') {
-            $('.bg-img').attr('src', url);
-        } else if (img === 'wm-file') {
-            $('.wm-img').attr('src', url);
-        }
-    };
-    
-});*/
+					fakeInput.val(data.name);			//вписывает имя файла в инпут
+					fileLoadWindow(data.rootUrl, fakeInputID);
+
+					console.log(data.width);
+					console.log(data.height);
+			}
+		})
+
+		function fileLoadWindow(url, img){
+			if (img === 'bg-file') {
+				$('.bg-img').attr('src', url);
+			} else if (img === 'wm-file') {
+				$('.wm-img').attr('src', url);
+			}
+		};
+	});
 
 
-    $(function () {
-        'use strict';
-        // Change this to the location of your server-side upload handler:
-        var url = './php/upload.php',
-            bgFile = $('#bg-file'),
-            bgFakeInput = bgFile.parent().find('.file-load__input-text'),
-            wmFile = $('#bg-file'),
-            wmFakeInput = bgFile.parent().find('.file-load__input-text');
-
-        $('#bg-file').fileupload({
-            url: url,
-            allowedTypes:"png,gif,jpg,jpeg",
-            dataType: 'json',
-            success: function (data) {
-                var fakeInputID = 'bg-file';
-
-                $('#bg-img-path').val(data.name);
-                console.log(data.name);
-                console.log(data.url);
-                console.log(data.width);
-                console.log(data.height);
-
-                fileLoadWindow(data.url, fakeInputID);
-            }
-        })
-
-        $('#wm-file').fileupload({
-            url: url,
-            allowedTypes:"png,gif,jpg,jpeg",
-            dataType: 'json',
-            success: function (data) {
-                var fakeInputID = 'wm-file';
-
-                $('#wm-img-path').val(data.name);
-                console.log(data.name);
-                console.log(data.url);
-                console.log(data.width);
-                console.log(data.height);
-
-                fileLoadWindow(data.url, fakeInputID);
-            }
-        })
-
-        function fileLoadWindow(url, img){
-            if (img === 'bg-file') {
-                $('.bg-img').attr('src', url);
-            } else if (img === 'wm-file') {
-                $('.wm-img').attr('src', url);
-            }
-        };
-    });
--- app/php/upload.php
+++ app/php/upload.php
@@ -1,52 +1,54 @@
 <?php 
 require_once 'functions.php';
 // устанавливаем путь к папке для загрузки
-$uploadDir = "../img/upload/";
+$rootDir = "img/upload/";         //путь для подстановки в index.html, сделано чтобы работало когда сайт лежит не только в корне
+$uploadDir = "../".$rootDir;    //путь для сохранения на сервере, относительно этого php файла
 // устанавливаем валидные MYME-types
 $types = array("image/gif", "image/png", "image/jpeg", "image/pjpeg", "image/x-png");
 // Устанавливаем максимальный размер файла
-$file_size = 2097152; // 2МБ
+$file_size = 15097152; // 2МБ
 // Получаем данные из глобального массива
 $file = $_FILES['files'];
 // Массив с результатами отработки скрипта
 $data = array();
 // Если размер файла больше максимально допустимого
 if($file['size'][0] > $file_size){
-    echo "Файл слишком большой. Загружать можно только изображения (gif|png|jpg|jpeg) размером до 2МБ";
-    exit;
-    $data['msg'] = "Файл слишком большой. Загружать можно только изображения (gif|png|jpg|jpeg) размером до 2МБ";
-    $data['url'] = '';
+	echo "Файл слишком большой. Загружать можно только изображения (gif|png|jpg|jpeg) размером до 2МБ";
+	exit;
+	$data['msg'] = "Файл слишком большой. Загружать можно только изображения (gif|png|jpg|jpeg) размером до 2МБ";
+	$data['url'] = '';
 }
 // если MYME-type файла не соответствует допустимому
 else if(!in_array($file['type'][0], $types)){
-    echo "Загружать можно только изображения (gif|png|jpg|jpeg) размером до 2МБ";
-    exit;
-    $data['msg'] = "Загружать можно только изображения (gif|png|jpg|jpeg) размером до 2МБ";
-    $data['url'] = '';
+	echo "Загружать можно только изображения (gif|png|jpg|jpeg) размером до 2МБ";
+	exit;
+	$data['msg'] = "Загружать можно только изображения (gif|png|jpg|jpeg) размером до 2МБ";
+	$data['url'] = '';
 }
 // Если ошибок нет
 else if($file['error'][0] == 0){
-    // получаем имя файла
-    $filename = basename($file['name'][0]);
-    // получаем расширение файла
-    $extension = pathinfo($file['name'][0], PATHINFO_EXTENSION);
-    // перемещаем файл из временной папки в  нужную
-    if(move_uploaded_file($file['tmp_name'][0], $uploadDir.str2url($filename).'.'.$extension)){
-        $data['msg'] = "ОК";
-        $data['url'] = $uploadDir.str2url($filename).'.'.$extension;
-        $data['name'] = str2url($filename).'.'.$extension;
-        // получаем размеры файла
-        $size = getimagesize($data['url']);
-        $data['width'] = $size[0]; //ширина
-        $data['height'] = $size[1]; //высота
-    }
-    // ошибка при перемещении файла
-    else {
-        echo "Возникла неизвестная ошибка при загрузке файла";
-        exit;
-        $data['msg'] = "Возникла неизвестная ошибка при загрузке файла";
-        $data['url'] = '';
-    }
+	// получаем имя файла
+	$filename = basename($file['name'][0]);
+	// получаем расширение файла
+	$extension = pathinfo($file['name'][0], PATHINFO_EXTENSION);
+	// перемещаем файл из временной папки в  нужную
+	if(move_uploaded_file($file['tmp_name'][0], $uploadDir.str2url($filename).'.'.$extension)){
+		$data['msg'] = "ОК";
+		$data['url'] = $uploadDir.str2url($filename).'.'.$extension;    //нужен для обращения к картинке и получения ее размера
+		$data['rootUrl'] = $rootDir.str2url($filename).'.'.$extension;
+		$data['name'] = str2url($filename).'.'.$extension;
+		// получаем размеры файла
+		$size = getimagesize($data['url']);
+		$data['width'] = $size[0]; //ширина
+		$data['height'] = $size[1]; //высота
+	}
+	// ошибка при перемещении файла
+	else {
+		echo "Возникла неизвестная ошибка при загрузке файла";
+		exit;
+		$data['msg'] = "Возникла неизвестная ошибка при загрузке файла";
+		$data['url'] = '';
+	}
 }
 // Выводим результат в JSON и заверщаем в скрипт
 echo json_encode($data);
